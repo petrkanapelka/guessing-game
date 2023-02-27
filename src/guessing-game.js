756069d4de86be382c5bd0fabf6a1e48f63acd3c
petrkanapelka <kanapelka-petr@mail.ru>
2023-02-28 01:23:39 +0300

    feat: solve task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,6 +1,6 @@
 class GuessingGame {
     constructor(num) {
-        this.num = num;
+        //this.num = num;
     }
 
     setRange(min, max) {
@@ -8,7 +8,7 @@ class GuessingGame {
         this.great=max;
     }
     guess() {
-        this.average = Math.round ((this.great+this.low)/2);
+        this.average = Math.round (this.low+(this.great-this.low)/2);
         return this.average;
     }
     lower() {
